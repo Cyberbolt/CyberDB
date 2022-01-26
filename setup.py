8a877a42991bbfb743928a225e6458e7afe934a2
Cyberbolt <735245473@qq.com>
2022-01-27 00:38:58 +0800

    0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="CyberDB",
-    version="0.1.0",
+    version="0.1.3",
     author="Cyberbolt",
     author_email="dtconlyone@gmail.com",
     description="CyberDB is a main memory database of Python. You can use Dictionaries, Lists as data storage and it supports data persistence. Inter-process communication through Socket TCP has extremely high performance. In addition, you can customize your own data structure based on this module to support Gunicorn inter-process communication, distributed computing, model deployment of machine learning, etc.",
